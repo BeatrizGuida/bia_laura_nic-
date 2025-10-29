d1686e0fe88b600df47708fe1674bb77bd860ca7
BeatrizGuida <beatrizbpg@al.insper.edu.br>
2025-10-29 18:41:54 -0300

    commit.7(cores)

--- grid.py
+++ grid.py
@@ -5,6 +5,7 @@ class Grid:
         self.tamanho_celula= 30
         #inicia no zero a grid
         self.grid= [[0 for j in range(self.num_colunas)] for i in range(self.num_linhas)]
+        self.colors= self.get_cell_colors()
 
 
     def print_grid(self):
@@ -14,3 +15,15 @@ class Grid:
             #imprime valor linha por linha
             print()
     
+
+    def get_cell_colors(self):
+        cinza_escuro = (26, 31, 40)
+        verde = (47, 230, 23)
+        vermelho = (232, 18, 18)
+        laranja= (226, 116, 17)
+        amarelo = (237, 234, 4)
+        roxo = (166, 0, 247)
+        ciano = (21, 204, 209)
+        azul = (13, 64, 216)
+
+        return [cinza_escuro, verde, vermelho, laranja, amarelo, roxo, ciano, azul]
