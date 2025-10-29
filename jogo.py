1dcf2024a0a0e3434511279b78382fd83b8ede5f
laurapittoli <lauravpp@al.insper.edu.br>
2025-10-29 17:48:20 -0300

    commit.2

--- jogo.py
+++ jogo.py
@@ -2,7 +2,20 @@ import pygame
 
 pygame.init()
 
+#tamanho da tela
 tela= pygame.display.set_mode((300, 600))
+#nome do jogo
 pygame.display.set_caption('Trix!')
+# relogio para controlar a velocidade do jogo
+relogio = pygame.time.Clock()
 
-cronometro= pygame.time.Clock()
+#loop principal do jogo
+while True:
+    #verifica os eventos
+    for eventos in pygame.event.get():
+        if eventos.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    #pega todas as alterações feitas e desenha uma imagem nova
+    pygame.display.update()
+    relogio.tick(60)
