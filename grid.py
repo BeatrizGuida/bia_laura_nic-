f75e01b243e3daea8f39789413c1ac234e6bfbf8
laurapittoli <lauravpp@al.insper.edu.br>
2025-10-29 18:51:13 -0300

    commit.8(cores_neon_e_comemtários)

--- grid.py
+++ grid.py
@@ -5,7 +5,7 @@ class Grid:
         self.tamanho_celula= 30
         #inicia no zero a grid
         self.grid= [[0 for j in range(self.num_colunas)] for i in range(self.num_linhas)]
-        self.colors= self.get_cell_colors()
+        self.colors= self.cor_dos_blocos()
 
 
     def print_grid(self):
@@ -16,14 +16,14 @@ class Grid:
             print()
     
 
-    def get_cell_colors(self):
-        cinza_escuro = (26, 31, 40)
-        verde = (47, 230, 23)
-        vermelho = (232, 18, 18)
-        laranja= (226, 116, 17)
-        amarelo = (237, 234, 4)
-        roxo = (166, 0, 247)
-        ciano = (21, 204, 209)
-        azul = (13, 64, 216)
-
-        return [cinza_escuro, verde, vermelho, laranja, amarelo, roxo, ciano, azul]
+    def cor_dos_blocos(self):
+        #sete blocos de cores diferentes para as peças
+        cinza_escuro = (26, 31, 40)       
+        verde_neon   = (77, 255, 0)     
+        vermelho_neon = (255, 30, 30)     
+        laranja_neon = (255, 140, 0)      
+        amarelo_neon = (255, 255, 0)       
+        roxo_neon    = (191, 0, 255)       
+        ciano_neon   = (0, 255, 255)      
+        azul_neon    = (0, 100, 255)       
+        return [cinza_escuro, verde_neon, vermelho_neon, laranja_neon, amarelo_neon, roxo_neon, ciano_neon, azul_neon]
