0b0a7068238981abffe01df51f922c63f17e5010
BeatrizGuida <beatrizbpg@al.insper.edu.br>
2025-11-03 20:29:38 -0300

    commit.14

--- blocks.py
+++ blocks.py
@@ -62,7 +62,7 @@ class TBlock (Block):
 
 class ZBlock (Block):
     def __init__(self):
-        super().__init__(id = 1)
+        super().__init__(id = 7)
         self.cells = {
             0: [Position(0, 0), Position(0, 1), Position(1, 1), Position(1, 2)],
             1: [Position(0, 2), Position(1, 1), Position(1, 2), Position(2, 1)],
