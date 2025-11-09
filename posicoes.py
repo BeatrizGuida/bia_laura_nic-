d099e074e78b29435ed291f0d04e3403a16feb28
laurapittoli <lauravpp@al.insper.edu.br>
2025-11-09 12:10:49 -0300

    commit52

--- posicoes.py
+++ posicoes.py
@@ -2,3 +2,6 @@ class Position:
     def __init__(self, linha, coluna):
         self.linha= linha
         self.coluna= coluna
+    def mostrar_posicao(self):
+        return f"Linha: {self.linha}, Coluna: {self.coluna}"
+    
